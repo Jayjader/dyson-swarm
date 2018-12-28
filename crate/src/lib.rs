592232295450eaee4da9413e47a0feea85b22ba5
Noah Jay <njay47@gmail.com>
2018-12-28 23:10:23 +0100

    Remove Rust DOM manipulation

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -36,15 +36,5 @@ pub fn run() -> Result<(), JsValue> {
     
     let mut state = game::State::new();
 
-    let window = web_sys::window().expect("should have a Window");
-    let document = window.document().expect("should have a Document");
-
-    let p: web_sys::Node = document.create_element("p")?.into();
-    p.set_text_content(Some("Hello from Rust, WebAssembly, and Webpack!"));
-
-    let body = document.body().expect("should have a body");
-    let body: &web_sys::Node = body.as_ref();
-    body.append_child(&p)?;
-
     Ok(())
 }
