890051be56918ff61c628897d41c973a5aef3dcc
Noah Jay <njay47@gmail.com>
2018-12-28 23:31:40 +0100

    Add Babel & Preact dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,39 @@ const dist = path.resolve(__dirname, "dist");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-  entry: "./js/index.js",
-  output: {
-    path: dist,
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    }),
+    entry: "./js/index.js",
+    output: {
+        path: dist,
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: dist,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            ["transform-react-jsx", { "pragma": "h" }],
+                            ["syntax-dynamic-import"]
+                        ]
+                    }
+                }
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
 
-    new WasmPackPlugin({
-      crateDirectory: path.resolve(__dirname, "crate")
-    }),
-  ]
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, "crate")
+        }),
+    ]
 };
