92364ddc9876860e52213db7193e57f203959412
Noah Jay <njay47@gmail.com>
2018-12-31 14:12:39 +0100

    Refactor Rust into 1 file, add State::new() test
    Moved game.rs contents into lib.rs to allow #[wasm_bindgen] use on structs previously defined in game.rs

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -5,8 +5,6 @@ extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
-mod game;
-
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function to get better error messages if we ever panic.
@@ -29,12 +27,110 @@ cfg_if! {
     }
 }
 
-// Called by our JS entry point to run the example.
 #[wasm_bindgen]
-pub fn run() -> Result<(), JsValue> {
-    set_panic_hook();
-    
-    let mut state = game::State::new();
+#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
+pub enum Resource {
+    Electricity,
+    Ore,
+    Metal,
+    Satellite,
+}
 
-    Ok(())
+#[wasm_bindgen]
+#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
+pub enum Building {
+    SolarCollector,
+    Miner,
+    Refiner,
+    SatelliteFactory,
+    Launcher,
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+pub struct ResourceCount{
+    resource: Resource,
+    count: u64,
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+pub struct BuildingCount{
+    building: Building,
+    count: u64,
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+pub struct StateResources {
+    pub electricity: ResourceCount,
+    pub ore: ResourceCount,
+    pub metal: ResourceCount,
+    pub satellites: ResourceCount,
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+pub struct StateBuildings {
+    pub collectors: BuildingCount,
+    pub miners: BuildingCount,
+    pub refiners: BuildingCount,
+    pub satellite_factories: BuildingCount,
+}
+
+#[wasm_bindgen]
+pub struct State {
+    pub resources: StateResources,
+    pub buildings: StateBuildings,
+}
+
+#[wasm_bindgen]
+impl State {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> State {
+        State {
+            resources: StateResources {
+                electricity: ResourceCount{resource: Resource::Electricity, count: 0},
+                ore: ResourceCount{resource: Resource::Ore, count: 0},
+                metal: ResourceCount{resource: Resource::Metal, count: 0},
+                satellites: ResourceCount{resource: Resource::Satellite, count: 0},
+            },
+            buildings: StateBuildings {
+                collectors: BuildingCount{building: Building::SolarCollector, count: 0},
+                miners: BuildingCount{building: Building::Miner, count: 0},
+                refiners: BuildingCount{building: Building::Refiner, count: 0},
+                satellite_factories: BuildingCount{building: Building::SatelliteFactory, count: 0},
+            },
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use State;
+    use Resource;
+    use Building;
+
+    #[test]
+    fn test_state_new() {
+        let state = State::new();
+        assert_eq!(state.resources.electricity.resource, Resource::Electricity);
+        assert_eq!(state.resources.ore.resource, Resource::Ore);
+        assert_eq!(state.resources.metal.resource, Resource::Metal);
+        assert_eq!(state.resources.satellites.resource, Resource::Satellite);
+        assert_eq!(state.resources.electricity.count, 0);
+        assert_eq!(state.resources.ore.count, 0);
+        assert_eq!(state.resources.metal.count, 0);
+        assert_eq!(state.resources.satellites.count, 0);
+
+        assert_eq!(state.buildings.collectors.building, Building::SolarCollector);
+        assert_eq!(state.buildings.miners.building, Building::Miner);
+        assert_eq!(state.buildings.refiners.building, Building::Refiner);
+        assert_eq!(state.buildings.satellite_factories.building, Building::SatelliteFactory);
+        assert_eq!(state.buildings.collectors.count, 0);
+        assert_eq!(state.buildings.miners.count, 0);
+        assert_eq!(state.buildings.refiners.count, 0);
+        assert_eq!(state.buildings.satellite_factories.count, 0);
+    }
 }
