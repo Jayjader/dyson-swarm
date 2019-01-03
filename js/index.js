dba55834215f0d459160a6f04457886f59b28cfa
Noah Jay <njay47@gmail.com>
2019-01-03 15:51:12 +0100

    Format, Cleanup index.js

--- js/index.js
+++ js/index.js
@@ -1,44 +1,40 @@
-import { h, render, Component } from 'preact';
+import {h, render, Component} from 'preact';
 import 'preact/devtools';
 
 import {ResourcesDisplay} from './displays/Resources.js';
 import {BuildingsDisplay} from './displays/Buildings.js';
 import {SwarmDisplay} from './displays/Swarm.js';
 
-import("../crate/pkg").then(wasm => {
-    let {
-        State,
-        Resource,
-        Building,
-    } = wasm;
+import('../crate/pkg').then(wasm => {
+    let { new_game_state } = wasm;
 
     class Game extends Component {
         constructor(props) {
             super(props);
             this.state = {
-                game: new State(),
+                game: new_game_state(),
             };
         }
 
         buildCollector = () => {
             this.setState(prevState => ({game: prevState.game.add_collector()}));
-        }
+        };
 
         buildMiner = () => {
             this.setState(prevState => ({game: prevState.game.add_miner()}));
-        }
+        };
 
         buildRefiner = () => {
             this.setState(prevState => ({game: prevState.game.add_refiner()}));
-        }
+        };
 
         buildSatelliteFactory = () => {
             this.setState(prevState => ({game: prevState.game.add_satellite_factory()}));
-        }
+        };
 
         buildLauncher = () => {
             this.setState(prevState => ({game: prevState.game.add_launcher()}));
-        }
+        };
 
         render() {
             return <div class="game-content">
@@ -46,37 +42,37 @@ import("../crate/pkg").then(wasm => {
                     <ResourcesDisplay resources={this.state.game.resources}/>
                     <BuildingsDisplay buildings={this.state.game.buildings}/>
                     <SwarmDisplay swarm={{count: 0}}/>
-            </div>
-            <div class="actions">
-                <button type="button"
-                    onClick={this.buildCollector}
-                >
-                    Build Solar Collector (5 Metal)
-                </button>
-                <button type="button"
-                    onClick={this.buildMiner}
-                >
-                    Build Miner (8 Metal)
-                </button>
-                <button type="button"
-                    onClick={this.buildRefiner}
-                >
-                    Build Refiner (25 Metal)
-                </button>
-                <button type="button"
-                    onClick={this.buildSatelliteFactory}
-                >
-                    Build Satellite Factory (15 Metal)
-                </button>
-                <button type="button"
-                    onClick={this.buildLauncher}
-                >
-                    Build Satellite Launcher (10 Metal)
-                </button>
-            </div>
-                </div>;
+                </div>
+                <div class="actions">
+                    <button type="button"
+                            onClick={this.buildCollector}
+                    >
+                        Build Solar Collector (5 Metal)
+                    </button>
+                    <button type="button"
+                            onClick={this.buildMiner}
+                    >
+                        Build Miner (8 Metal)
+                    </button>
+                    <button type="button"
+                            onClick={this.buildRefiner}
+                    >
+                        Build Refiner (25 Metal)
+                    </button>
+                    <button type="button"
+                            onClick={this.buildSatelliteFactory}
+                    >
+                        Build Satellite Factory (15 Metal)
+                    </button>
+                    <button type="button"
+                            onClick={this.buildLauncher}
+                    >
+                        Build Satellite Launcher (10 Metal)
+                    </button>
+                </div>
+            </div>;
         }
     }
 
-    render(<Game/>, document.getElementById("app"));
+    render(<Game/>, document.getElementById('app'));
 });
