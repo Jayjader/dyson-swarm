7b7d9750db9df22bae56eaf1602c03c0f8d05d33
Noah Jay <njay47@gmail.com>
2022-10-03 23:56:14 +0200

    add structure / layout for storage overview

---
+++ src/overview/Storage.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let name: string;
+  export let icon: string;
+  export let unit: string = "";
+  export let stored: number;
+  export let produced: number;
+  export let consumed: number;
+  $: total = (produced ?? 0) - (consumed ?? 0);
+</script>
+
+<div
+  class="flex flex-row gap-1 border-4 rounded border-slate-500 bg-zinc-200 text-slate-700"
+>
+  <img class="max-h-12 aspect-square self-center mr-1" alt={name} src={icon} />
+  <div class="flex-grow flex flex-row">
+    <div class="flex-grow flex flex-col">
+      <h4 class="font-bold">{name}</h4>
+      <h5 class="font-bold">Stored</h5>
+      <output>{stored}{unit}</output>
+    </div>
+    <div class="flex-grow flex flex-col">
+      <h5 class="text-center font-bold">Last Tick</h5>
+      <div class="flex flex-row justify-around gap-1">
+        <div class="basis-1/2 flex flex-col">
+          <h5 class="font-bold">Total</h5>
+          <output>{total > 0 ? "+" : ""}{total}{unit}</output>
+        </div>
+        <div class="basis-1/2 flex flex-col">
+          <span class="flex flex-row gap-1">
+            <output>{produced}{unit}</output><span>Produced</span>
+          </span>
+          <span class="flex flex-row gap-1">
+            <output>{consumed}{unit}</output><span>Consumed</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/overview/StorageOverview.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import Storage from "./Storage.svelte";
+
+  const watt = "W";
+  const wattTick = `${watt}t`;
+  const kilogram = "kg";
+</script>
+
+<section class="flex flex-col gap-1 p-1 border-2 rounded border-slate-100">
+  <h3 class="text-slate-100 font-bold text-center">Storage</h3>
+  <Storage
+    name="Power"
+    icon="/electric.svg"
+    unit={wattTick}
+    stored={150}
+    produced={30}
+    consumed={24}
+  />
+  <Storage
+    name="Ore"
+    icon="/ore.svg"
+    unit={kilogram}
+    stored={150}
+    produced={13}
+    consumed={12}
+  />
+  <Storage
+    name="Metal"
+    icon="/metal-bar.svg"
+    unit={kilogram}
+    stored={5}
+    produced={2}
+    consumed={5}
+  />
+  <Storage
+    name="Satellites"
+    icon="/cardboard-box.svg"
+    stored={1}
+    produced={0}
+    consumed={0}
+  />
+</section>
