cf162c8a4a0f72dd1cce1036374eb7e75dae6845
Noah Jay <njay47@gmail.com>
2022-10-05 23:26:28 +0200

    better storage overview display logic

--- src/overview/Storage.svelte
+++ src/overview/Storage.svelte
@@ -3,25 +3,34 @@
   export let icon: string;
   export let unit: string = "";
   export let stored: number;
-  export let produced: number;
-  export let consumed: number;
-  $: total = (produced ?? 0) - (consumed ?? 0);
+  export let produced: number = 0;
+  export let consumed: number = 0;
+  $: total = produced - consumed;
 </script>
 
 <div
-  class="flex flex-row gap-1 border-4 rounded border-slate-500 bg-zinc-200 text-slate-700"
+  class="flex flex-row flex-wrap gap-1 p-1 border-4 rounded border-slate-500 bg-zinc-200 text-slate-700"
 >
-  <img class="max-h-12 aspect-square self-center mr-1" alt={name} src={icon} />
-  <div class="flex-grow flex flex-row">
-    <div class="flex-grow flex flex-col">
-      <h4 class="font-bold">{name}</h4>
+  <img
+    class="max-h-12 aspect-square self-center px-1 ml-auto mr-auto"
+    alt={name}
+    src={icon}
+  />
+  <div class="flex-grow flex flex-row flex-wrap">
+    <div class="flex flex-col items-start">
+      <h4
+        class="flex-grow font-bold border-b-2 border-b-slate-500 rounded-br pr-1"
+        style="line-height: 1;"
+      >
+        {name}
+      </h4>
       <h5 class="font-bold">Stored</h5>
       <output>{stored}{unit}</output>
     </div>
     <div class="flex-grow flex flex-col">
-      <h5 class="text-center font-bold">Last Tick</h5>
-      <div class="flex flex-row justify-around gap-1">
-        <div class="basis-1/2 flex flex-col">
+      <h5 class="text-center font-bold" style="line-height: 1;">Last Tick</h5>
+      <div class="flex flex-row gap-1">
+        <div class="basis-1/2 flex flex-col items-end">
           <h5 class="font-bold">Total</h5>
           <output>{total > 0 ? "+" : ""}{total}{unit}</output>
         </div>
