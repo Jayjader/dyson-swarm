23b43860f02b2ac654d1bfd104abbaef9b925c17
Noah Jay <njay47@gmail.com>
2022-10-09 22:11:11 +0200

    fix grid circuit breaker styling

--- src/overview/GridBreaker.svelte
+++ src/overview/GridBreaker.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  export let open: boolean;
-  $: color = open ? "red" : "sky";
+  export let open: boolean = false;
+  $: border = open ? "border-red-400" : "border-blue-400";
+  $: text = open ? `text-red-400` : "text-blue-400";
 </script>
 
 <label
-  class="basis-full border-8 rounded-xl flex flex-col justify-center items-stretch text-center border-{color}-400 text-{color}-400"
+  class="basis-full border-8 rounded-xl {border} pt-1 pb-2 flex flex-col justify-center items-stretch text-center {text} cursor-pointer"
 >
   Circuit Breaker
-  <input type="checkbox" checked={open} />
+  <input type="checkbox" readonly bind:checked={open} class="cursor-pointer" />
 </label>
+
+<style>
+  input::before,
+  input::after {
+    /* see https://css-tricks.com/fun-times-styling-checkbox-states/ */
+  }
+</style>
