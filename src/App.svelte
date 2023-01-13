4638040fe9e9ae9b48900865ecba0a1e56914e07
Noah Jay <njay47@gmail.com>
2023-01-13 05:23:27 +0100

    implement rudimentary save slot name persistence in localStorage

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,31 @@
 <script lang="ts">
   import {
-    uiStore,
-    isAtTitle,
-    isAtSaveFromTitle,
-    isInSimulation,
+    APP_UI_CONTEXT,
     type AppUiState,
     isAtSaveFromSimulation,
-    APP_UI_CONTEXT,
+    isAtSaveFromTitle,
+    isAtTitle,
+    isInSimulation,
+    uiStore,
   } from "./appStateStore";
   import Simulation from "./Simulation.svelte";
   import { setContext } from "svelte";
-  import { createMemoryStream } from "./events/processes/eventStream";
-  import { createClock } from "./events/processes/clock";
   import SaveSlots from "./SaveSlots.svelte";
+
   let uiStack: AppUiState;
   uiStore.subscribe((stack: AppUiState) => {
     uiStack = stack;
   });
   setContext(APP_UI_CONTEXT, { uiStore });
-
-  const SAVE_SLOTS = {
-    autoSave: null,
-    slots: [
-      {
-        name: "my super awesome first save",
-        processors: [createMemoryStream(), createClock(0)],
-      },
-    ],
-  } as const;
 </script>
 
 {#if isAtTitle(uiStack)}
   <main
-    class="overflow-y-scroll m-2 flex flex-col justify-between rounded border-2 bg-slate-200 px-2"
+    class="m-2 flex flex-col justify-between overflow-y-scroll rounded border-2 bg-slate-200 px-2"
   >
-    <h1 style="font-size: clamp(2rem, 2dvh, initial)">Dyson Swarm Operator Training Simulator</h1>
+    <h1 style="font-size: clamp(2rem, 2dvh, initial)">
+      Dyson Swarm Operator Training Simulator
+    </h1>
     <div class="flex flex-col gap-2 self-center">
       <button
         class="self-stretch rounded border-2 border-slate-900 px-2"
@@ -55,11 +46,11 @@
     </div>
   </main>
 {:else if isAtSaveFromTitle(uiStack)}
-  <SaveSlots saveSlots={SAVE_SLOTS} />
+  <SaveSlots />
 {:else if isInSimulation(uiStack)}
   <Simulation simulation={uiStack[1]} />
 {:else if isAtSaveFromSimulation(uiStack)}
-  <SaveSlots saveSlots={SAVE_SLOTS} />
+  <SaveSlots />
 {/if}
 
 <style>
--- src/SaveSlots.svelte
+++ src/SaveSlots.svelte
@@ -1,25 +1,107 @@
 <script lang="ts">
   import NavButton from "./NavButton.svelte";
-  import { getContext, onDestroy } from "svelte";
+  import { getContext, onDestroy, onMount } from "svelte";
   import { APP_UI_CONTEXT, simulationIsLoaded } from "./appStateStore";
   import type { SaveState } from "./events";
 
-  type Save = { name: string } & SaveState;
-  export let saveSlots: { autoSave: null | Save; slots: Save[] } = {
+  enum Slot {
+    AUTO = "auto",
+    NAMES = "slots",
+    NAME,
+  }
+  const slotStorageKey = (s: Slot) => {
+    switch (s) {
+      case Slot.NAMES:
+      case Slot.AUTO:
+        // this is only a function because I couldn't find a way to make the compiler
+        // understand that passing one of these as argument here would guarantee directly
+        // being a string
+        return () => `save_slots--${s}`;
+      case Slot.NAME:
+        return (index: number) => `save_slots--${index}`;
+    }
+  };
+  type SaveStub = { name: string };
+  type Save = SaveStub & SaveState;
+  type SaveStubs = {
+    slots: SaveStub[];
+    autoSave: null | { name: "AUTOSAVE" };
+  };
+
+  let saveStubs: SaveStubs = {
     autoSave: null,
     slots: [],
   };
+
+  function readStubs(): SaveStubs {
+    const existingAutosave = window.localStorage.getItem(
+      slotStorageKey(Slot.AUTO)()
+    );
+    // debugger
+    const autoSave =
+      existingAutosave === null ? null : JSON.parse(existingAutosave);
+    const slotNames = window.localStorage.getItem(slotStorageKey(Slot.NAMES)());
+    const slots =
+      slotNames === null
+        ? []
+        : [...new Set(JSON.parse(slotNames))].map((name) => ({ name }));
+    // console.debug({ autoSave, slots });
+    return { autoSave, slots };
+  }
+
+  function readStub(name: string): null | Save {
+    const data = window.localStorage.getItem(slotStorageKey(Slot.NAME)(name));
+    return data === null ? null : (JSON.parse(data) as Save);
+  }
+
+  function writeSlotToStorage(save: Save) {
+    if (save.name === "AUTOSAVE") {
+      window.localStorage.setItem(
+        slotStorageKey(Slot.AUTO)(),
+        JSON.stringify(save.processors)
+      );
+    } else {
+      window.localStorage.setItem(
+        slotStorageKey(Slot.NAME)(save.name),
+        JSON.stringify(save.processors)
+      );
+      const NAMES_KEY = slotStorageKey(Slot.NAMES)();
+      const names = window.localStorage.getItem(NAMES_KEY);
+      const namesArray = new Set(names === null ? [] : JSON.parse(names));
+      namesArray.add(save.name);
+      window.localStorage.setItem(NAMES_KEY, JSON.stringify([...namesArray]));
+    }
+  }
+
   let slotIndex = -2;
   let inSimulation = false;
+  let simulation = null;
   const uiStore = getContext(APP_UI_CONTEXT).uiStore;
+  let simSub = null;
   const uiSub = uiStore.subscribe((stack) => {
     inSimulation = simulationIsLoaded(stack);
+    if (inSimulation) {
+      simSub = stack[1].subscribe((sim) => {
+        simulation = sim;
+      });
+    } else if (simSub !== null) {
+      simSub();
+      simSub = null;
+    }
+  });
+  onDestroy(() => {
+    if (simSub !== null) simSub();
   });
   onDestroy(uiSub);
 
   const selectSlot = (index: number) => {
     slotIndex = index;
   };
+
+  onMount(() => {
+    saveStubs = readStubs();
+  });
+  $: disabled = slotIndex === -2;
 </script>
 
 <main
@@ -53,11 +135,13 @@
     <button
       class:focus={slotIndex === -1}
       class={"w-full flex-grow rounded-xl border-2 border-slate-900 font-mono" +
-        (saveSlots.autoSave !== null ? " bg-stone-400" : "")}
-      on:click={selectSlot.bind(this, -1)}>AUTOSAVE</button
+        (saveStubs.autoSave !== null ? " bg-stone-400" : "")}
+      on:click={selectSlot.bind(this, -1)}
+      >{#if saveStubs.autoSave === null}(NO
+      {/if}AUTOSAVE{#if saveStubs.autoSave === null}){/if}</button
     >
     <hr class="basis-2/3 rounded border-2 border-slate-900" />
-    {#each saveSlots.slots as save, i}
+    {#each saveStubs.slots as save, i}
       <button
         class:focus={slotIndex === i}
         class="w-full flex-grow flex-grow rounded-xl border-2 border-slate-900 bg-stone-400"
@@ -65,9 +149,9 @@
       >
     {/each}
     <button
-      class:focus={slotIndex === saveSlots.slots.length}
+      class:focus={slotIndex === saveStubs.slots.length}
       class="w-full flex-grow flex-grow rounded-xl border-2 border-slate-900"
-      on:click={selectSlot.bind(this, saveSlots.slots.length)}
+      on:click={selectSlot.bind(this, saveStubs.slots.length)}
       >(New Slot)</button
     >
   </div>
@@ -75,7 +159,16 @@
     {#if inSimulation}
       <button
         class="rounded border-2 border-slate-900 disabled:border-dashed"
-        disabled={slotIndex === -2}>Save</button
+        {disabled}
+        on:click={() => {
+          const name = "a test save -=#=- " + String(Math.random());
+          writeSlotToStorage({
+            name,
+            processors: simulation.processors,
+          });
+          saveStubs.slots.push({ name });
+          slotIndex = -2;
+        }}>Save</button
       >
     {:else}
       <div>
@@ -84,23 +177,23 @@
     {/if}
     <button
       class="rounded border-2 border-slate-900 disabled:border-dashed"
-      disabled={slotIndex === -2}>Import</button
+      {disabled}>Import</button
     >
     <button
       class="rounded border-2 border-slate-900 disabled:border-dashed"
-      disabled={slotIndex === -2}>Delete</button
+      {disabled}>Delete</button
     >
     <button
       class="rounded border-2 border-slate-900 disabled:border-dashed"
-      disabled={slotIndex === -2}>Load</button
+      {disabled}>Load</button
     >
     <button
       class="rounded border-2 border-slate-900 disabled:border-dashed"
-      disabled={slotIndex === -2}>Export</button
+      {disabled}>Export</button
     >
     <button
       class="rounded border-2 border-slate-900 disabled:border-dashed"
-      disabled={slotIndex === -2}>Clone</button
+      {disabled}>Clone</button
     >
   </div>
 </main>
