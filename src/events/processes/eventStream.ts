552956e39010e18f134dfbd35d316468d0ceefdb
Noah Jay <njay47@gmail.com>
2023-01-29 07:45:25 +0100

    minor refactor - better function argument name

--- src/events/processes/eventStream.ts
+++ src/events/processes/eventStream.ts
@@ -99,7 +99,7 @@ export function getEventStream(simulation: Simulation) {
 
 export function getTickEvents(
   stream: EventStream,
-  n: number
+  tick: number
 ): BusEvent[] | undefined {
-  return stream.data.received.get(n);
+  return stream.data.received.get(tick);
 }
