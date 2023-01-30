1162b94d380abf40dd0f583fda0ac912e753b699
Noah Jay <njay47@gmail.com>
2023-01-30 20:25:31 +0100

    fix mouse events for remove/unwrap build queue items

--- src/panels/fabricator/BuildQueueItem.svelte
+++ src/panels/fabricator/BuildQueueItem.svelte
@@ -1,43 +1,78 @@
 <script lang="ts">
   import { getContext } from "svelte";
   import { BUILD_QUEUE_STORE } from "./store";
-  import NestableButton from "./NestableButton.svelte";
 
   export let isRepeat;
   export let position: [number, ...number[]];
   const { mode, uiState } = getContext(BUILD_QUEUE_STORE);
+  let isButton = false,
+    onClick;
+  const callback = {
+    "remove-build-order": uiState.removeBuildOrder,
+    "remove-repeat-order": uiState.removeRepeatOrder,
+    "unwrap-repeat-order": uiState.unwrapRepeatOrder,
+  };
+  mode.subscribe((tag) => {
+    isButton =
+      (isRepeat &&
+        (tag === "remove-repeat-order" || tag === "unwrap-repeat-order")) ||
+      (!isRepeat && tag === "remove-build-order");
+    onClick = isButton ? callback[tag]?.bind(this, position) : undefined;
+  });
+
+  let hovering = false;
+  $: mouseover = () => {
+    if (!hovering) {
+      hovering = true;
+    }
+  };
+  $: mouseout = () => {
+    if (hovering) {
+      hovering = false;
+    }
+  };
+
+  $: classes = [
+    isButton
+      ? hovering
+        ? "border-rose-600"
+        : "border-rose-500"
+      : isRepeat
+      ? "border-violet-200"
+      : "border-sky-500",
+    isButton && hovering
+      ? "bg-rose-800"
+      : isRepeat && !hovering
+      ? "bg-sky-800"
+      : "bg-sky-300",
+    isRepeat ? "items-start " : "",
+  ].join(" ");
 </script>
 
-<li
-  class="flex flex-col gap-1 rounded border-2 bg-sky-300 text-gray-900"
-  class:remove={$mode === "remove-build-order"}
-  class:items-start={isRepeat}
-  class:border-sky-500={!isRepeat}
-  class:border-violet-200={isRepeat}
-  class:bg-sky-800={isRepeat}
-  class:border-double={!isRepeat}
-  class:border-rose-500={$mode === "remove-build-order"}
-  class:hover:border-rose-600={!isRepeat && $mode === "remove-build-order"}
-  class:active:border-rose-600={!isRepeat && $mode === "remove-build-order"}
-  class:hover:bg-rose-600={!isRepeat && $mode === "remove-build-order"}
->
-  <!--  <div>position: {position}</div>-->
-  {#if isRepeat && $mode === "remove-repeat-order"}
-    <NestableButton on:click={uiState.removeRepeatOrder.bind(this, position)}>
-      <slot />
-    </NestableButton>
-  {:else if isRepeat && $mode === "unwrap-repeat-order"}
-    <NestableButton on:click={uiState.unwrapRepeatOrder.bind(this, position)}>
-      <slot />
-    </NestableButton>
-  {:else if !isRepeat && $mode === "remove-build-order"}
-    <NestableButton on:click={uiState.removeBuildOrder.bind(this, position)}>
-      <slot />
-    </NestableButton>
-  {:else}
+{#if !isButton}
+  <li class="flex flex-col gap-1 rounded-md border-2 text-slate-800 {classes}">
     <slot />
-  {/if}
-</li>
+  </li>
+{:else}
+  <li
+    class="flex flex-col gap-1 rounded-md border-2 text-slate-800 {classes} hover:cursor-pointer"
+    class:active:border-rose-600={hovering}
+    tabindex="0"
+    on:mouseover|stopPropagation={mouseover}
+    on:mouseout|stopPropagation={mouseout}
+    on:click|stopPropagation={onClick}
+  >
+    <!--todo: focus semantics; might be waiting on https://bugzilla.mozilla.org/show_bug.cgi?id=1494196-->
+    <button
+      style="display: contents"
+      on:mouseover|stopPropagation={mouseover}
+      on:mouseout|stopPropagation={mouseout}
+      on:click|stopPropagation={onClick}
+    >
+      <slot />
+    </button>
+  </li>
+{/if}
 
 <style>
   li {
