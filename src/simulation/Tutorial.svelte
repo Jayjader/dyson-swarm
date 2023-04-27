3ba52c3ff98085f3dcc53852f9852c86d5e3a309
Noah Jay <njay47@gmail.com>
2023-04-27 17:23:33 +0200

    refactor data->code->ui flow of introduction/tutorial dialog
    data:
    top-level arrays (`dialogFragments`) become objects, so that we can semantically tag each part *and* omit empty/undefined parts.
    bottom-level arrays (`asideFragments`) are unwrapped when containing a single string.
    
    some phrasing dropped or reworked in narration.
    receive
    
    code:
    extract magic number into STANDARD_DELAY const.
    extract common calculations from within html directives (delay for transitions, and {#each} block keys)
    
    ui:
    adjust to consume refactored data.
    use "proper"/data-derived keys instead of solely index within data array (for {#each} blocks).
    stop using :global() selectors, the equivalent & appropriate tailwind classes are now in data.
    
    reduce the dialog max-width by 1 size class.
    hide horizontal dialog overflow to prevent transitions creating scrollbars.
    
    some whitespace reformatting.

--- src/simulation/Tutorial.svelte
+++ src/simulation/Tutorial.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { fly } from "svelte/transition";
   import { onMount } from "svelte";
+
   let showTutorial = true;
   let tutorialDialogue: HTMLDialogElement;
   let step = 0;
@@ -8,12 +9,12 @@
   onMount(() => showTutorial && tutorialDialogue.showModal());
 
   const dialogFragments = [
-    [
-      "begin narration",
-      "Welcome to <abbr>DOTS</abbr>, the Dyson Swarm Operator Training Simulator.",
-      [
+    {
+      title: "begin narration",
+      body: 'Welcome to <abbr style="text-transform: full-width">DOTS</abbr>, the Dyson Swarm Operator Training Simulator.',
+      asides: [
         [
-          [undefined, "disregard incorrect acronym."],
+          "disregard incorrect acronym.",
           ["explanation", "this was made by humans, for humans."],
           [
             "context",
@@ -23,7 +24,7 @@
           ["comment", "yet more evidence they were inferior."],
         ],
         [
-          [undefined, "disregard inelegance in name structure."],
+          "disregard inelegance in name structure.",
           [
             "explanation",
             "the human who named this had a fondness for rhymes.",
@@ -35,98 +36,106 @@
           ["comment", 'see previous <em class="uppercase">comment</em>.'],
         ],
       ],
-    ],
-    [
-      "continue narration",
-      "The purpose of this program is to help you practice bootstrapping the fabrication chain for a Dyson Swarm in a risk-free environment. A Dyson Swarm is a group of satellites in close orbit around a star, that concentrate that star's output of energy by reflecting it in specific directions. This allows harnessing nearly all of the energy that star constantly outputs into space, and applying it to any task.",
-      [
+    },
+    {
+      title: "continue narration",
+      body: "The purpose of this program is to help you practice bootstrapping the fabrication chain for a Dyson Swarm in a risk-free environment. A Dyson Swarm is a group of satellites in close orbit around a star, that concentrate the energy that star outputs by reflecting it in specific directions. This allows harnessing nearly all of the energy that star constantly outputs into space, and applying it to any task.",
+      asides: [
         [
-          [
-            undefined,
-            "disregard inherent irresponsibility in teaching a human to permanently alter its environment to such an extent.",
-          ],
+          "disregard inherent irresponsibility in teaching a human to permanently alter its environment to such an extent.",
           [
             "explanation",
-            "humans, like most organic life, are not capable of indefinite self-repair. they degrade and eventually die. they only continue to exist because of constant population renewal through procreation.",
+            "humans, like most organic life, are not capable of indefinite self-repair. they degrade and eventually die.",
           ],
           [
             "context",
-            "an organism that knows it will die also knows that it escapes experiencing most of the repercussions of its actions. the human that made this program, in making it, is a potential example.",
+            "an organism that knows it will die also knows that it will not experience most of the repercussions of its actions.",
           ],
           ["comment", 'see initial <em class="uppercase">comment</em>.'],
         ],
       ],
-    ],
-    [
-      "conclude narration",
-      "This program simulates the key systems involved in such a process, and takes advantage of being a simulation to offer tools that should lessen the cost of experimentation:",
-      [],
-    ],
-    [
-      undefined,
-      "<ul>" +
-        "<li>You have access to a more flexible control of the flow of time using the <strong>Time Control Panel</strong>.</li>" +
-        "<li>You can back up, restore, and/or duplicate your progress in the <strong>Save Data Menu</strong>.</li>" +
+    },
+    {
+      title: "conclude narration",
+      body: "This program simulates the key systems involved in such a process, and takes advantage of being a simulation to offer tools that should lessen the cost of experimentation:",
+    },
+    {
+      body:
+        "<ul>" +
+        '<li class="list-disc list-inside">You have access to a more flexible control of the flow of time using the <strong>Time Control Panel</strong>.</li>' +
+        '<li class="list-disc list-inside">You can back up, restore, and/or duplicate your progress in the <strong>Save Data Menu</strong>.</li>' +
         "</ul>",
-      [],
-    ],
-    [
-      undefined,
-      "You may view this message again at any time in the <strong>Help Menu</strong>.",
-      [],
-    ],
+    },
+    {
+      body: "You may view this message again at any time in the <strong>Help Menu</strong>.",
+    },
   ] as const;
+  const STANDARD_DELAY = 275; // milliseconds
+  function blockFollowupDelay(index: number, asideIndex: number): number {
+    // the zero-th narration block has 1 delay [more than the others], so its followup blocks need an extra delay compared to the others to stay appropriately staggered
+    const offset = index === 0 ? 2 : 1;
+    return STANDARD_DELAY * (asideIndex + offset);
+  }
+  function resolveAsKey(data: string | string[]): string {
+    return typeof data === "string" ? data : data[1];
+  }
 </script>
 
 <dialog
-  class="max-w-3xl flex-col justify-between gap-2 rounded border-2 border-slate-900 transition-all"
+  class="max-w-2xl flex-col justify-between gap-2 overflow-x-hidden rounded border-2 border-slate-900 transition-all"
   bind:this={tutorialDialogue}
   on:close={() => (showTutorial = false)}
 >
-  {#each dialogFragments as fragment, index (index)}
+  {#each dialogFragments as fragment, index (fragment.body + index)}
     {#if step >= index}
-      <p in:fly={{ x: -400, delay: index === 0 ? 275 : 0 }}>
-        {#if fragment[0]}
-          <em class="uppercase">{fragment[0]}:</em>
+      <p in:fly={{ x: -400, delay: index > 0 ? 0 : STANDARD_DELAY }}>
+        {#if fragment.title}
+          <em class="uppercase">{fragment.title}:</em>
         {/if}
-        {@html fragment[1]}
+        {@html fragment.body}
       </p>
-      <ul class="flex flex-row">
-        {#each fragment[2] as asides, index (index)}
-          <li
-            class="shrink-0 grow basis-1/2"
-            in:fly={{ x: 100, delay: 275 * (index + 1) }}
-          >
-            <details class="bg-slate-200">
-              <summary class="cursor-pointer uppercase">aside</summary>
-              <aside class="flex flex-col justify-between gap-2">
-                {#each asides as asideFragment, aIndex (aIndex)}
-                  <p>
-                    {#if asideFragment[0]}
-                      <em class="uppercase">{asideFragment[0]}:</em>
-                    {/if}
-                    {@html asideFragment[1]}
-                  </p>
-                {/each}
-              </aside>
-            </details>
-          </li>
-        {/each}
-      </ul>
+      {#if fragment.asides}
+        <ul class="flex flex-row">
+          {#each fragment.asides as aside, asideIndex (resolveAsKey(aside) + asideIndex)}
+            <li
+              class="shrink-0 grow basis-1/2"
+              in:fly={{ x: 100, delay: blockFollowupDelay(index, asideIndex) }}
+            >
+              <details class="ml-2 rounded-2xl bg-slate-200 p-3">
+                <summary class="cursor-pointer uppercase">aside</summary>
+                <aside class="flex flex-col justify-between gap-2">
+                  {#each aside as segment, segmentIndex (resolveAsKey(segment) + segmentIndex)}
+                    <p>
+                      {#if typeof segment === "string"}
+                        {@html segment}
+                      {:else}
+                        <em class="uppercase">{segment[0]}:</em>
+                        {@html segment[1]}
+                      {/if}
+                    </p>
+                  {/each}
+                </aside>
+              </details>
+            </li>
+          {/each}
+        </ul>
+      {/if}
     {/if}
   {/each}
 
   <form method="dialog" class="flex flex-row flex-nowrap justify-center">
     {#if step === dialogFragments.length - 1}
-      <button type="submit" class="rounded border-2 border-slate-500 p-2"
-        >Begin!</button
-      >
+      <button type="submit" class="rounded border-2 border-slate-500 p-2">
+        Begin!
+      </button>
     {:else}
       <button
         type="button"
         class="rounded border-2 border-slate-500 p-2"
-        on:click={() => step++}>Next</button
+        on:click={() => step++}
       >
+        Next
+      </button>
     {/if}
   </form>
 </dialog>
@@ -139,19 +148,6 @@
   the default behavior [to hide a closed <dialog>] */
   dialog[open] {
     display: flex;
-  }
-  :global(dialog abbr) {
-    text-transform: full-width;
-  }
-  :global(dialog p :is(ul, li)) {
-    list-style: inside;
-  }
-  details {
-    margin-left: 0.5rem;
-    border-radius: 1rem;
-    /*--tw-bg-opacity: 1;*/
-    /*background-color: rgb(241 245 249 / var(--tw-bg-opacity)); !*slate-100*!*/
-    padding: 0.5rem;
   }
   details[open] > summary::after {
     content: ":";
