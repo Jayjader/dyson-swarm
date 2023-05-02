f126ed503eeadbfde33e7a92733602d48d3e4b8c
Noah Jay <njay47@gmail.com>
2023-05-02 16:23:01 +0200

    style working count toggle buttons' disabled state as "pushed-in and/or active" (in construct overview panel)

--- src/panels/constructs/WorkingCountToggle.svelte
+++ src/panels/constructs/WorkingCountToggle.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <button
-  class="rounded border-2 border-zinc-50 px-1 disabled:bg-slate-700 disabled:border-dashed disabled:border-zinc-400"
+  class="rounded border-2 border-zinc-50 bg-transparent px-1 text-zinc-900 disabled:border-zinc-900 disabled:bg-zinc-900 disabled:text-slate-100"
   {disabled}
   on:click><slot /></button
 >
