0c33eb0cd2018384c2fe1568ad6c0343a2bdea7b
Noah Jay <njay47@gmail.com>
2024-09-28 19:18:49 +0200

    refactor <App/> state store to newly async'ed Simulation store
    flatten AppStateStore's internal array into basically a set of flags and expose domain methods instead of delegating that to its callers. the domain feels settled enough that appropriate method names have been found. this allows a better encapsulation between components (presentation and interactions) and data wrangling.
    
    pull code inside html event handlers into script block functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-  import {
-    APP_UI_CONTEXT,
-    MainMenu,
-    makeAppStateStore,
-    SaveMenu,
-    SettingsMenu,
-    SimMenu,
-  } from "./appStateStore";
+  import { APP_UI_CONTEXT, makeAppStateStore } from "./appStateStore";
   import Simulation from "./simulation/Simulation.svelte";
   import { setContext } from "svelte";
   import SaveSlots from "./save/SaveSlots.svelte";
@@ -14,27 +7,58 @@
   import SimulationMenu from "./main-menu/SimulationMenu.svelte";
   import Settings from "./settings/Settings.svelte";
   import { makeSettingsStore, SETTINGS_CONTEXT } from "./settings/store";
-  import {
-    makeObjectiveTracker,
-    type ObjectiveTracker,
-  } from "./objectiveTracker/store";
-  import { makeSimulationStore } from "./events";
-  import Introduction from "./Introduction.svelte";
   import { getOrCreateSqlWorker } from "./events/sqlWorker";
-  import { type Adapters, initSqlAdapters } from "./adapters";
+  import {
+    type Adapters,
+    initInMemoryAdapters,
+    initSqlAdapters,
+  } from "./adapters";
 
   const settings = makeSettingsStore();
   setContext(SETTINGS_CONTEXT, { settings });
-  const appStateStack = makeAppStateStore(settings);
-  setContext(APP_UI_CONTEXT, { appStateStack });
-  let objectiveTracker: ObjectiveTracker;
+  const appStateStore = makeAppStateStore(settings);
+  setContext(APP_UI_CONTEXT, { appStateStore });
 
-  let showIntro = false;
+  const showIntro = false;
 
-  let adapters: Adapters | undefined;
+  const inMemory: boolean = true;
+  async function startNewSimulation() {
+    let adapters: Adapters | undefined;
+    if (inMemory) {
+      adapters = initInMemoryAdapters();
+    } else {
+      adapters = initSqlAdapters(await getOrCreateSqlWorker());
+    }
+    await appStateStore.startNewSim(
+      adapters,
+      window.performance.now(),
+      showIntro,
+    );
+  }
+
+  function closeSettings(event: CustomEvent) {
+    if (event.detail === "exit-sim") {
+      appStateStore.exitSettingsAndSim();
+    } else {
+      appStateStore.closeSettings();
+    }
+  }
 </script>
 
-{#if $appStateStack.at(-1) === MainMenu}
+{#if $appStateStore.inMenu}
+  <SimulationMenu
+    on:open-save={appStateStore.openSave}
+    on:open-settings={appStateStore.openSettings}
+    on:close={closeSettings}
+  />
+{:else if $appStateStore.inSettings}
+  <Settings
+    on:clear-progress={appStateStore.clearProgress}
+    on:close={appStateStore.closeSettings}
+  />
+{:else if $appStateStore.inSave}
+  <SaveSlots />
+{:else if $appStateStore.simulation === undefined}
   <main
     class="m-2 flex flex-col justify-between overflow-y-scroll rounded border-2 bg-slate-200 px-2"
   >
@@ -47,30 +71,15 @@
     <div class="flex flex-col gap-2 self-center">
       <button
         class="self-stretch rounded border-2 border-slate-900 px-2"
-        on:click={() =>
-          getOrCreateSqlWorker().then((sqlWorker) => {
-            appStateStack.pop();
-            objectiveTracker = makeObjectiveTracker();
-            objectiveTracker.setActive([0]); // Intro
-            adapters = initSqlAdapters(sqlWorker);
-            // adapters = initInMemoryAdapters(new Map());
-            appStateStack.push(
-              makeSimulationStore(objectiveTracker, adapters).loadNew(
-                window.performance.now(),
-              ),
-              objectiveTracker,
-            );
-            showIntro = true;
-          })}>Start New Simulation</button
+        on:click={startNewSimulation}>Start New Simulation</button
       >
       <button
         class="self-stretch rounded border-2 border-slate-900 px-2"
-        on:click={() => appStateStack.push(SaveMenu)}
-        >Use Existing Simulation</button
+        on:click={appStateStore.openSave}>Use Existing Simulation</button
       >
       <button
         class="self-stretch rounded border-2 border-slate-900 px-2"
-        on:click={() => appStateStack.push(SettingsMenu)}>Settings</button
+        on:click={appStateStore.openSettings}>Settings</button
       >
       <a
         class="text-slate-700 hover:underline hover:visited:text-slate-500"
@@ -82,33 +91,10 @@
       </a>
     </div>
   </main>
-{:else if $appStateStack.at(-1) === SimMenu}
-  <SimulationMenu
-    on:open-save={() => appStateStack.push(SaveMenu)}
-    on:open-settings={() => appStateStack.push(SettingsMenu)}
-    on:close={(event) =>
-      event.detail === "exit-sim"
-        ? (appStateStack.pop(3), appStateStack.push(MainMenu))
-        : appStateStack.pop()}
-  />
-{:else if $appStateStack.at(-1) === SettingsMenu}
-  <Settings
-    on:clear-progress={$appStateStack.at(2)?.clearProgress}
-    on:close={() => appStateStack.pop()}
-  />
-{:else if $appStateStack.at(-1) === SaveMenu}
-  <SaveSlots />
 {:else}
-  {#if showIntro && objectiveTracker}
-    <Introduction
-      fragments={objectiveTracker.objectives[0].details}
-      on:close={() => (showIntro = false)}
-    />
-  {/if}
   <Simulation
-    simulation={$appStateStack[1]}
-    objectives={$appStateStack[2]}
-    on:open-menu={() => appStateStack.push(SimMenu)}
+    simulation={$appStateStore.simulation}
+    on:open-menu={appStateStore.openMenu}
   />
 {/if}
 
--- src/appStateStore.ts
+++ src/appStateStore.ts
@@ -1,52 +1,101 @@
 import { writable } from "svelte/store";
-import type { SimulationStore } from "./events";
+import { makeSimulationStore, type SimulationStore } from "./events";
 import type { SettingsStore } from "./settings/store";
-import type { ObjectiveTracker } from "./objectiveTracker/store";
+import { makeObjectiveTracker } from "./objectiveTracker/store";
+import type { Adapters } from "./adapters";
 
-export const MainMenu = Symbol("main menu");
-export const SettingsMenu = Symbol("settings menu");
-export const SaveMenu = Symbol("save slots menu");
-export const SimMenu = Symbol("in-simulation menu");
-
-type Stack =
-  | [SettingsStore, typeof MainMenu]
-  | [SettingsStore, typeof MainMenu, typeof SettingsMenu]
-  | [SettingsStore, typeof MainMenu, typeof SaveMenu]
-  | [SettingsStore, SimulationStore, ObjectiveTracker]
-  | [SettingsStore, SimulationStore, ObjectiveTracker, typeof SimMenu]
-  | [
-      SettingsStore,
-      SimulationStore,
-      ObjectiveTracker,
-      typeof SimMenu,
-      typeof SettingsMenu
-    ]
-  | [
-      SettingsStore,
-      SimulationStore,
-      ObjectiveTracker,
-      typeof SimMenu,
-      typeof SaveMenu
-    ];
+type BaseAppState = {
+  settings: SettingsStore;
+};
+type AppState = BaseAppState & {
+  simulation: SimulationStore | undefined;
+  inMenu: boolean;
+  inSettings: boolean;
+  inSave: boolean;
+};
 
 export function makeAppStateStore(settings: SettingsStore) {
-  const { subscribe, update } = writable<Stack>([settings, MainMenu]);
+  const { subscribe, update } = writable<AppState>({
+    settings,
+    simulation: undefined,
+    inMenu: false,
+    inSettings: false,
+    inSave: false,
+  });
   return {
     subscribe,
-    push: (...items: any[]) =>
-      update((stack) => {
-        stack.push(...items);
-        return stack;
-      }),
-    pop: (count: number = 1) => {
-      let popped;
-      update((stack) => {
-        popped = stack.splice(stack.length - count, count);
-        return stack;
+    openSave() {
+      update((state) => {
+        state.inSave = true;
+        return state;
       });
-      return popped;
+    },
+    closeSave() {
+      update((state) => {
+        state.inSave = false;
+        return state;
+      });
+    },
+    openSettings() {
+      update((state) => {
+        state.inSettings = true;
+        return state;
+      });
+    },
+    exitSettingsAndSim() {
+      update(({ settings }) => {
+        return {
+          settings,
+          simulation: undefined,
+          inMenu: false,
+          inSettings: false,
+          inSave: false,
+        };
+      });
+    },
+    closeSettings() {
+      update((state) => {
+        state.inSettings = false;
+        return state;
+      });
+    },
+    openMenu() {
+      update((state) => {
+        state.inMenu = true;
+        return state;
+      });
+    },
+    async startNewSim(
+      adapters: Adapters,
+      outsideClockTick: DOMHighResTimeStamp,
+      showIntro: boolean,
+    ) {
+      const objectiveTracker = makeObjectiveTracker();
+      if (showIntro) {
+        objectiveTracker.setActive([0]); // Intro
+      }
+      const simulationStore = await makeSimulationStore(
+        objectiveTracker,
+        adapters,
+      ).loadNew(outsideClockTick);
+      update(({ settings }) => {
+        return {
+          settings,
+          simulation: simulationStore,
+          inMenu: false,
+          inSettings: false,
+          inSave: false,
+        };
+      });
+    },
+    clearProgress() {
+      update((state) => {
+        state.simulation?.objectives.clearProgress();
+        return state;
+      });
     },
   };
 }
+export type AppStateStore = ReturnType<typeof makeAppStateStore>;
 
 export const APP_UI_CONTEXT = Symbol("app ui store");
